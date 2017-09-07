d411915dd888ac7d7336873087369509e70e5074
kisw3399 <31675773+kisw3399@users.noreply.github.com>
2017-09-07 23:56:29 +0900

    P_1st_assignment
    프로그래밍연습2

---
+++ name.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+ printf("My student id numberis 2017301022. \n");
+ printf("My name is Tae Beom Kim ±èÅÂ¹ü. \n");
+ printf("My mobile phone number is 010 9298 7859. \n");
+ printf("My GitHub address is kisw3399@naver.com \n");
+
+ return 0;
+}
